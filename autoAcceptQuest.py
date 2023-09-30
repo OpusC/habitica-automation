1ff4fdb048a1d9e3dbfb891c017926947be2a787
Jason Callaghan <77325743+OpusC@users.noreply.github.com>
2023-09-30 13:43:21 -0700

    Update autoAcceptQuest.py

--- autoAcceptQuest.py
+++ autoAcceptQuest.py
@@ -3,11 +3,9 @@ import requests
 
 url = "https://habitica.com/api/v3/groups/party"
 
-xapiuser = ""
-
-xclient =  ""
-
-xapikey = ""
+xapiuser = "YOUR_API_USER_ID"
+xclient =  "YOUR_CLIENT_ID"
+xapikey = "YOUR_API_KEY"
 
 headers = {'content-type': 'application/json',
         'x-api-user': xapiuser,
